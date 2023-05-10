c3df5aab1b7bf65f2938195a91dadf8853a1b63b
Checoman7 <sergio.gar.gonz@gmail.com>
2023-05-10 13:52:10 -0600

    good format to pomxml?
    Signed-off-by: Checoman7 <sergio.gar.gonz@gmail.com>

--- src/main/java/com/dnylpz/apiDemo/GreetingController.java
+++ src/main/java/com/dnylpz/apiDemo/GreetingController.java
@@ -3,7 +3,6 @@ package com.dnylpz.apiDemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -12,7 +11,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
