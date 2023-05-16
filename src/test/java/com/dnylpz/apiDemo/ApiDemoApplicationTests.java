07ddabae98eed8c6f3e97c51fe5601f4a7246268
Checoman7 <sergio.gar.gonz@gmail.com>
2023-05-15 19:56:38 -0600

    Quitando lo de Selenium
    Signed-off-by: Checoman7 <sergio.gar.gonz@gmail.com>

--- src/test/java/com/dnylpz/apiDemo/ApiDemoApplicationTests.java
+++ src/test/java/com/dnylpz/apiDemo/ApiDemoApplicationTests.java
@@ -1,13 +1,7 @@
 package com.dnylpz.apiDemo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,19 +15,6 @@ class ApiDemoApplicationTests {
 
 	@Value("chrome,firefox,edge")
 	private List<String> browsers;
-
-	//@Autowired
-	//private WebDriver webDriver;
-
-	/*@Test
-	public void testGoogleSearch() {
-		webDriver.get(appUrl);
-		WebElement searchBox = webDriver.findElement(By.name("q"));
-		searchBox.sendKeys("Spring Boot");
-		searchBox.sendKeys(Keys.RETURN);
-		WebElement results = webDriver.findElement(By.id("search"));
-		assertTrue(results.isDisplayed());
-	}*/
 
 	@Test
 	public void testAppIsRunning() {
